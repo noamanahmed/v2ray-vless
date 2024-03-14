f730ca648e7905bbd255c75318278781d6298705
Nauman Ahmed <nauman.ahmed@nevron.tv>
2024-03-14 10:56:22 +0500

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # v2ray-vless
-A repository to setup VRay+
+A repository to setup VRay+VLess.
+
+
+## Getting Started
+Clone this repository to your local machine:
+
+```sh
+git clone https://github.com/noamanahmed/v2ray-vless
+```
+
+Navigate to the project directory:
+
+```sh
+cd v2ray-vless
+```
+
+(Optional) Copy config.dist.json to config.json and default.dist.json to default.json and overrwride and configuration you desire
+
+Run this installer command
+
+```sh
+./install.sh
+```
+This will generate a QR Code and will also save a file named qr.png in the directory.
+
+## Android Application
+
+You can use V2RayNG Android Application to scan the QR Code generated. 
+Github Link (https://github.com/2dust/v2rayNG)
+PlayStore Link (https://play.google.com/store/apps/details?id=com.v2ray.ang)
---
+++ install.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+set -eo pipefail
+shopt -s nullglob
+
+if [ "$EUID" -ne 0 ]
+then echo "Please run as root"
+    exit
+fi
+
+if ! command -v xray &> /dev/null; then    
+    echo "Xray is not installed. Installing..."
+    bash -c "$(curl -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh)" @ install --beta
+else
+    echo "Xray is already installed."
+fi
+
+
+if [ ! -f "config.json" ]; then  
+    cp "config.dist.json" "config.json"    
+fi
+
+if [ ! -f "default.json" ]; then  
+    cp "default.dist.json" "default.json"    
+fi
+
+# Extract the desired variables using jq
+name=$(jq -r '.name' default.json)
+email=$(jq -r '.email' default.json)
+port=$(jq -r '.port' default.json)
+sni=$(jq -r '.sni' default.json)
+path=$(jq -r '.path' default.json)
+json=$(cat config.json)
+
+keys=$(xray x25519)
+pk=$(echo "$keys" | awk '/Private key:/ {print $3}')
+pub=$(echo "$keys" | awk '/Public key:/ {print $3}')
+serverIp=$(curl -s ipv4.wtfismyip.com/text)
+uuid=$(xray uuid)
+shortId=$(openssl rand -hex 8)
+
+url="vless://$uuid@$serverIp:$port?type=http&security=reality&encryption=none&pbk=$pub&fp=chrome&path=$path&sni=$sni&sid=$shortId#$name"
+
+newJson=$(echo "$json" | jq \
+    --arg pk "$pk" \
+    --arg uuid "$uuid" \
+    --arg port "$port" \
+    --arg sni "$sni" \
+    --arg path "$path" \
+    --arg email "$email" \
+    '.inbounds[0].port= '"$(expr "$port")"' |
+     .inbounds[0].settings.clients[0].email = $email |
+     .inbounds[0].settings.clients[0].id = $uuid |
+     .inbounds[0].streamSettings.realitySettings.dest = $sni + ":443" |
+     .inbounds[0].streamSettings.realitySettings.serverNames += ["'$sni'", "www.'$sni'"] |
+     .inbounds[0].streamSettings.realitySettings.privateKey = $pk |
+     .inbounds[0].streamSettings.realitySettings.shortIds += ["'$shortId'"]')
+     
+echo "$newJson" | sudo tee /usr/local/etc/xray/config.json >/dev/null
+
+echo ""
+echo "$url"
+echo ""
+
+qrencode -s 120 -t ANSIUTF8 "$url"
+qrencode -s 50 -o qr.png "$url"
